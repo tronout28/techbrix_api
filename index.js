08a797df1524f884cc5054cdbb63b18a28b18278
tronout28 <fatihabdurahman28@gmail.com>
2023-11-15 14:59:19 +0700

    techbrix api

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const db = require('./connection')
+const response = require('./response')
+
+
+//route utama get 
+app.get('/', (req, res) => {
+    db.query((error, result) => {
+       response(200, result, " http://localhost:3000/headset ,/handphone, /laptop", res)
+    })
+})
+
+app.get('/headset', (req, res) => {
+    const sql = "SELECT * FROM headset"
+    db.query( sql ,(error, result) => {
+       response(200, result, "data from headset", res)
+    })
+})
+
+app.get('/laptop', (req, res) => {
+    const sql = "SELECT * FROM laptop"
+    db.query( sql ,(error, result) => {
+       response(200, result, "data from headset", res)
+    })
+})
+
+app.get('/handphone', (req, res) => {
+    const sql = "SELECT * FROM handphone"
+    db.query( sql ,(error, result) => {
+       response(200, result, "data from headset", res)
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
